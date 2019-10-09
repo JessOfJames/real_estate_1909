74a034282d00b163908c63b7ede770c043f09ddb
Jesse Higgins <jessofjames@gmail.com>
2019-10-09 09:43:54 -0600

    Adds area instance variable, area attr_reader, and calculate_room_area method.

--- lib/room.rb
+++ lib/room.rb
@@ -1,9 +1,14 @@
 class Room
-  attr_reader :category, :length, :width
+  attr_reader :category, :length, :width, :area
 
   def initialize(category, length, width)
     @category = category
     @length = length
     @width = width
+    @area = @width * @length
+  end
+
+  def calculate_room_area
+    @area
   end
 end
