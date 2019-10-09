11161b1aefe201d27a84c87e6c7e9758ac21a95d
Jesse Higgins <jessofjames@gmail.com>
2019-10-09 09:41:59 -0600

    Adds test for category, length, width, and area. All tests pass.

--- test/room_test.rb
+++ test/room_test.rb
@@ -10,4 +10,20 @@ class RoomTest < Minitest::Test
   def test_it_exists
     assert_instance_of Room, @room
   end
+
+  def test_it_has_a_category
+    assert_equal :bedroom, @room.category
+  end
+
+  def test_it_has_a_length
+    assert_equal 10, @room.length
+  end
+
+  def test_it_has_a_width
+    assert_equal 13, @room.width
+  end
+
+  def test_it_has_an_area
+    assert_equal 130, @room.calculate_room_area
+  end
 end
